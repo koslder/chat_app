e45001119670f37f930a57e94fd69154642752e2
koslder <franciscoecojedo2002@gmail.com>
2025-05-27 20:13:05 +0800

    When to use stateless or statefull

--- lib/counter_stateful_demo.dart
+++ lib/counter_stateful_demo.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CounterStateful extends StatefulWidget {
-  Color buttonColor = Colors.blue;
+  final Color buttonColor;
 
   const CounterStateful({Key? key, required this.buttonColor})
       : super(key: key);

88e6feca3b6db7268a4ee747b25f17e689cc8a95
koslder <franciscoecojedo2002@gmail.com>
2025-05-30 03:48:02 +0800

    Exception handling

--- lib/widgets/picker_body.dart
+++ lib/widgets/picker_body.dart
@@ -4,8 +4,10 @@ import '../models/image_model.dart';
 import '../repo/image_repository.dart';
 
 class NetworkImagePickerBody extends StatelessWidget {
+  final Function(String) onImageSelected;
   NetworkImagePickerBody({
     Key? key,
+    required this.onImageSelected,
   }) : super(key: key);
 
   final ImageRepository _imageRepo = ImageRepository();
@@ -16,11 +18,15 @@ class NetworkImagePickerBody extends StatelessWidget {
         future: _imageRepo.getNetworkImages(),
         builder: (BuildContext context,
             AsyncSnapshot<List<PixelfordImage>> snapshot) {
-          if (snapshot.hasData)
+          if (snapshot.hasData) {
             return GridView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
-                return Image.network(snapshot.data![index].urlFullSize);
+                return GestureDetector(
+                    onTap: () {
+                      onImageSelected(snapshot.data![index].urlFullSize);
+                    },
+                    child: Image.network(snapshot.data![index].urlFullSize));
               },
               gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
                 crossAxisSpacing: 2,
@@ -28,7 +34,12 @@ class NetworkImagePickerBody extends StatelessWidget {
                 maxCrossAxisExtent: MediaQuery.of(context).size.width * 0.5,
               ),
             );
-          //return Image.network(snapshot.data![0].urlFullSize);
+          } else if (snapshot.hasError) {
+            return Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text('This is the error: ${snapshot.error}'),
+            );
+          }
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Center(child: CircularProgressIndicator()),
